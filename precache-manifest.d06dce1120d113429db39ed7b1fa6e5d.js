9b2cd25677ecd829c3f32aa918739c9416f451b4
Jesus Salatiel <jesussalatiel@hotmail.com>
2021-02-14 20:47:58 -0600

    Updates

---
+++ precache-manifest.d06dce1120d113429db39ed7b1fa6e5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19666be974f4c23a025db210e5bbc61",
+    "url": "/jesussalatiel/index.html"
+  },
+  {
+    "revision": "6ef27c20ecc2173c16e1",
+    "url": "/jesussalatiel/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "8f1c7e333390abd6bb5c",
+    "url": "/jesussalatiel/static/css/main.17710be9.chunk.css"
+  },
+  {
+    "revision": "6ef27c20ecc2173c16e1",
+    "url": "/jesussalatiel/static/js/2.6a7a1aa9.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/jesussalatiel/static/js/2.6a7a1aa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f1c7e333390abd6bb5c",
+    "url": "/jesussalatiel/static/js/main.7e7cd8c8.chunk.js"
+  },
+  {
+    "revision": "3262588dfc62c21d3704",
+    "url": "/jesussalatiel/static/js/runtime-main.4e0e59ff.js"
+  }
+]);
